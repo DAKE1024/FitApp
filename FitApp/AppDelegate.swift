5b0333c71121e34a085cb8203cd0eb330a0c4003
Даулетбай Комекбаев <dauletbajkomekbaev@MacBook-Air-Dauletbaj.local>
2023-06-28 20:17:57 +0500

    delete storyboard

--- FitApp/AppDelegate.swift
+++ FitApp/AppDelegate.swift
@@ -9,14 +9,18 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
